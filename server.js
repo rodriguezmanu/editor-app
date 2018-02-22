fbe385714ac1a94a9a010e6dfa641f753444fe59
Manuel Rodriguez <rodriguez.manu@gmail.com>
2018-02-22 15:39:16 -0300

    Adding server express

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/dist'));
+const app = express();
 
-app.listen(process.env.PORT || 8080);
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
